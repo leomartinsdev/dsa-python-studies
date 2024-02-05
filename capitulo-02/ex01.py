668abc998ad7828bc89f18048d3af6e4c7789e17
Leonardo Martins <leonardogustavo@outlook.com>
2024-02-05 17:37:20 -0300

    Initial Commit

---
+++ capitulo-02/ex01.py
@@ -0,0 +1,9 @@
+# Exercício 2.1 Converta as seguintes expressões matemáticas para que possam
+# ser calculadas usando o interpretador Python.
+# 10 + 20 X 30
+# 41 .;. 30
+# (94 + 2) X 6 · 1
+
+print(10 + 20 * 30)
+print(4**2 / 30)
+print((9**4 + 2) * 6 - 1)
