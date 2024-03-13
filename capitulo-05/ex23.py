b1a7fddc13db5ad242741655cb1c83a426d2ab95
Leonardo Martins <leonardogustavo@outlook.com>
2024-03-13 19:30:55 -0300

    Chapter 5 - 23

---
+++ capitulo-05/ex23.py
@@ -0,0 +1,24 @@
+while True:
+    numero = 19
+    counter = 2
+
+    if numero == 2:
+        print("O número é primo!")
+        break
+
+    if (numero % counter) == 0:
+        print("O número não é primo.")
+        break
+
+    counter += 1
+
+    while counter <= numero:
+        if counter == numero:
+            print("O número é primo!")
+            break
+        elif (numero % counter) == 0:
+            print("O número não é primo.")
+            break
+        else:
+            counter += 1
+    break
