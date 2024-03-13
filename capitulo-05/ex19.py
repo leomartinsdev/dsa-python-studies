8244da0d065778479d4149ff765052f3284bbeeb
Leonardo Martins <leonardogustavo@outlook.com>
2024-03-13 18:50:00 -0300

    Chapter 5 - 18, 19, 21

--- capitulo-05/ex19.py
+++ capitulo-05/ex19.py
@@ -1,6 +1,6 @@
 valor = int(input("Digite o valor a pagar: "))
 cédulas = 0
-atual = 50
+atual = 100
 apagar = valor
 
 while True:
@@ -8,7 +8,11 @@ while True:
         apagar -= atual
         cédulas += 1
     else:
-        print(f"{cédulas} cédula(s) de R${atual}")
+        if atual >= 1:
+            print(f"{cédulas} cédula(s) de R${atual}")
+        else:
+            print(f"{cédulas} moedas de R$ {atual}")
+
         if apagar == 0:
             break
 
@@ -22,4 +26,14 @@ while True:
             atual = 5
         elif atual == 5:
             atual = 1
+        elif atual == 1:
+            atual = 0.50
+        elif atual == 0.50:
+            atual = 0.10
+        elif atual == 0.10:
+            atual = 0.05
+        elif atual == 0.05:
+            atual = 0.02
+        elif atual == 0.02:
+            atual = 0.01
         cédulas = 0
