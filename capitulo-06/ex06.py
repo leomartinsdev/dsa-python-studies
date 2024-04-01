4ede24dff4d4f72a4072f1739e5f777180da141a
Leonardo Martins <leonardogustavo@outlook.com>
2024-04-01 17:29:27 -0300

    Chapter 06 ex 06

---
+++ capitulo-06/ex06.py
@@ -0,0 +1,46 @@
+último_fila1 = 10
+último_fila2 = 10
+fila1 = list(range(1, último_fila1 + 1))
+fila2 = list(range(1, último_fila2 + 1))
+
+while True:
+    print(f"\nExistem {len(fila1)} clientes na fila 1 e")
+    print(f"\n {len(fila2)} clientes na fila 2")
+    print(f"Fila 1 atual: {fila1}")
+    print(f"Fila 2 atual: {fila2}")
+    print("Digite F para adicionar um cliente ao fim da fila 1,")
+    print("ou G para adicionar um cliente ao fim da fila 2.")
+    print("\nDigite A para realizar o atendimento na fila 1 ou B para")
+    print("atendimento na fila 2. S para sair.")
+
+    operação = list(input("Operação (F, A, S): "))
+
+    index = 0
+
+    while index < len(operação):
+        if operação[index] == "A":
+            if len(fila1) > 0:
+                atendido = fila1.pop(0)
+                print(f"Cliente {atendido} atendido")
+            else:
+                print("Fila vazia, ninguém para atender.")
+            index += 1
+        elif operação[index] == "B":
+            if len(fila2) > 0:
+                atendido = fila2.pop(0)
+                print(f"Cliente {atendido} atendido")
+            else:
+                print("Fila vazia, ninguém para atender.")
+            index += 1
+        elif operação[index] == "F":
+            último_fila1 += 1  # Incrementa o ticket do novo cliente
+            fila1.append(último_fila1)
+            index += 1
+        elif operação[index] == "G":
+            último_fila2 += 1  # Incrementa o ticket do novo cliente
+            fila2.append(último_fila2)
+            index += 1
+        elif operação[index] == "S":
+            break
+        else:
+            print("Operação inválida")
